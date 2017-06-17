b25dce36841bf687155a8786ba31788d3b88112c
huhuanming <workboring@gmail.com>
2017-06-17 17:43:05 +0800

    Mock Fetch

--- config/jest-setup.js
+++ config/jest-setup.js
@@ -11,6 +11,5 @@ const { Response, Headers, Request } = require('whatwg-fetch');
 global.Response = Response;
 global.Headers = Headers;
 global.Request = Request;
-global.fetch = require('jest-fetch-mock');
 
 module.exports = react;
